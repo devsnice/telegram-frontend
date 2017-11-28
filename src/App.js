954a7818603fca3f99ab809209bf7f0ff30ba0c1
Borisov Semen <devsnice@gmail.com>
2017-11-28 18:28:07 +0300

    [*] - init create react application project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Auth from './components/containers/Auth/Auth';
+
+class App extends Component {
+  render() {
+    return <Auth />;
+  }
+}
+
+export default App;
